4fde2daad7f227472a0552a97d1b098f952c99ec
mataney <mataneyal1@gmail.com>
2019-03-11 22:25:42 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='rouge',
+      version='0.1',
+      description="This is a cloned version of google-research's rouge.",
+      url="https://github.com/matan/googles_rouge",
+      author='Matan Eyal',
+      author_email='mataneyal1@gmail.com',
+      packages=[],
+      zip_safe=False)
